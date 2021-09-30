7809c4fddd7561751f885fba7cffcd72c5675bc1
Mgamerz <mgamerz2@gmail.com>
2021-09-29 18:32:32 -0600

    SeqActLogger: Resolve external variables. Needs some testing to see if this is actually required...

--- SeqActLog Enabler/LE1SeqActLogEnabler.cpp
+++ SeqActLog Enabler/LE1SeqActLogEnabler.cpp
@@ -21,10 +21,11 @@ SPI_PLUGINSIDE_ASYNCATTACH;
 ME3TweaksASILogger logger("SeqAct_Log Enabler v2", "SeqActLog.txt");
 ScreenLogger screenLogger(L"SeqAct_Log Enabler v2");
 
+std::wstringstream ss;
 
-std::wstringstream& operator<<(std::wstringstream& ss, const FString& fStr)
+std::wstringstream& operator<<(std::wstringstream& ss, FString& fStr)
 {
-	
+
 	for (auto i = 0; i < fStr.Count && fStr(i) != 0; i++)
 	{
 		ss << fStr(i);
@@ -32,107 +33,173 @@ std::wstringstream& operator<<(std::wstringstream& ss, const FString& fStr)
 	return ss;
 }
 
+
+USequenceVariable* ResolveSeqVar_External(USeqVar_External* seqExtObj)
+{
+	// Look for parent sequence, find matching var link
+	USequence* parentSeq = seqExtObj->ParentSequence;
+	if (parentSeq != nullptr)
+	{
+		// Enumerate over the parent sequence objects
+		for (int i = 0; i < parentSeq->VariableLinks.Count; i++)
+		{
+			if (!strcmp(parentSeq->VariableLinks.Data[i].LinkVar.GetName(), seqExtObj->VarName.GetName())) // Find matching link
+			{
+				// Find matching linked variable
+				auto link = parentSeq->VariableLinks.Data[i];
+				for (int j = 0; j < link.LinkedVariables.Count; j++) {
+					auto variable = link.LinkedVariables.Data[j];
+					if (variable != nullptr)
+					{
+						// See if this is also an external variable, in which case we have to go up further
+						if (variable->IsA(USeqVar_External::StaticClass()))
+						{
+							return ResolveSeqVar_External(static_cast<USeqVar_External*>(variable));
+						}
+						else
+						{
+							// This is the object we're looking for!
+							return variable;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return nullptr; // Not found
+}
+
+
 // ProcessEvent hook
 // ======================================================================
 
 typedef void (*tProcessEvent)(UObject* Context, UFunction* Function, void* Parms, void* Result);
 tProcessEvent ProcessEvent = nullptr;
-
 tProcessEvent ProcessEvent_orig = nullptr;
+
+
+void PrintSequenceVariable(USequenceVariable* seqVar)
+{
+	// Exteranl goes first cause of some UE3 black magic where it acts like an object.
+	auto className = seqVar->Class->GetName();
+	if (seqVar->IsA(USeqVar_External::StaticClass()))
+	{
+		auto seqExtObj = static_cast<USeqVar_External*>(seqVar);
+		auto referencedObj = ResolveSeqVar_External(seqExtObj);
+		if (referencedObj != nullptr)
+		{
+			PrintSequenceVariable(referencedObj);
+		}
+		else
+		{
+			ss << L"SeqVar_External couldn't resolve variable ";
+			ss << seqExtObj->VariableLabel.Data;
+		}
+	}
+	else	if (seqVar->IsA(USeqVar_String::StaticClass()))
+	{
+		ss << static_cast<USeqVar_String*>(seqVar)->StrValue << " ";
+	}
+	else if (seqVar->IsA(USeqVar_Float::StaticClass()))
+	{
+		ss << static_cast<USeqVar_Float*>(seqVar)->FloatValue << " ";
+	}
+	else if (seqVar->IsA(USeqVar_Int::StaticClass()))
+	{
+		ss << static_cast<USeqVar_Int*>(seqVar)->IntValue << " ";
+	}
+	else if (seqVar->IsA(USeqVar_Bool::StaticClass()))
+	{
+		ss << (static_cast<USeqVar_Bool*>(seqVar)->bValue ? "True" : "False") << " ";
+	}
+	else if (seqVar->IsA(USeqVar_Object::StaticClass()))
+	{
+		auto seqVarObj = static_cast<USeqVar_Object*>(seqVar);
+		auto referencedObj = seqVarObj->ObjValue;
+		if (referencedObj != nullptr)
+		{
+			ss << referencedObj->GetName() << " ";
+		}
+	}
+	else if (seqVar->IsA(USeqVar_Name::StaticClass()))
+	{
+		ss << static_cast<USeqVar_Name*>(seqVar)->NameValue.GetName() << " ";
+	}
+	else
+	{
+		ss << L"Unknown SeqVar type: ";
+		ss << seqVar->Class->GetName();
+	}
+}
+
 void ProcessEvent_hook(UObject* Context, UFunction* Function, void* Parms, void* Result)
 {
-    if(!strcmp(Function->GetFullName(), "Function Engine.SequenceOp.Activated") && Context->IsA(USeqAct_Log::StaticClass()))
-    {
-        const auto seqLog = reinterpret_cast<USeqAct_Log*>(Context);
-        const auto numVarLinks = seqLog->VariableLinks.Count;
-        std::wstringstream ss;
+	if (!strcmp(Function->GetFullName(), "Function Engine.SequenceOp.Activated") && Context->IsA(USeqAct_Log::StaticClass()))
+	{
+		ss = std::wstringstream(); // Make new because i have no idea what i'm doing
+		auto seqLog = reinterpret_cast<USeqAct_Log*>(Context);
+		auto numVarLinks = seqLog->VariableLinks.Count;
 
-        // Todo: Patch the function that removes m_aObjComment
-        /*if(seqLog->bOutputObjCommentToScreen == 1ul)
-        {
-            const auto numLines = seqLog->m_aObjComment.Num();
-            for(auto k = 0; k < numLines; k++)
-            {
-                ss << seqLog->m_aObjComment(k) << " ";
-            }
-        }*/
+		// Todo: Patch the function that removes m_aObjComment
+		if (seqLog->bOutputObjCommentToScreen == 1ul)
+		{
+			auto numLines = seqLog->m_aObjComment.Num();
+			for (auto k = 0; k < numLines; k++)
+			{
+				ss << seqLog->m_aObjComment(k).Data;
+				ss << L" ";
+			}
+		}
 
-        for(auto i = 0; i < numVarLinks; i++)
-        {
-            const auto numVars = seqLog->VariableLinks(i).LinkedVariables.Count;
-            for(auto j = 0; j < numVars; j++)
-            {
-                auto seqVar = seqLog->VariableLinks(i).LinkedVariables(j);
-                if(seqVar->IsA(USeqVar_String::StaticClass()))
-                {
-                    ss << static_cast<USeqVar_String*>(seqVar)->StrValue << " ";
-                }
-                else if(seqVar->IsA(USeqVar_Float::StaticClass()))
-                {
-                    ss << static_cast<USeqVar_Float*>(seqVar)->FloatValue << " ";
-                }
-                else if(seqVar->IsA(USeqVar_Int::StaticClass()))
-                {
-                    ss << static_cast<USeqVar_Int*>(seqVar)->IntValue << " ";
-                }
-                else if(seqVar->IsA(USeqVar_Bool::StaticClass()))
-                {
-                    ss << (static_cast<USeqVar_Bool*>(seqVar)->bValue ? "True" : "False") << " ";
-                }
-                else if (seqVar->IsA(USeqVar_Object::StaticClass()))
-				{
-					const auto seqVarObj = static_cast<USeqVar_Object*>(seqVar);
-					auto referencedObj = seqVarObj->ObjValue;
-					if (referencedObj != nullptr)
-					{
-						ss << referencedObj->GetName() << " ";
-					}
-				}
-                else if (seqVar->IsA(USeqVar_Name::StaticClass()))
-				{
-					ss << static_cast<USeqVar_Name*>(seqVar)->NameValue.GetName() << " ";
-				}
-            }
-            
-        }
-        const wstring msg = ss.str();
-        writeln(L"SeqAct_Log: %s", msg.c_str());
-        if(msg.size() > 0)
-        {
-            logger.writeToLog(msg.c_str(), true, true);
-            screenLogger.LogMessage(msg);
-        }
-    }
-    else if(!strcmp(Function->GetFullName(), "Function SFXGame.BioHUD.PostRender"))
-    {
-        const auto hud = static_cast<ABioHUD*>(Context);
-        screenLogger.PostRenderer(hud);
-    }
-    ProcessEvent_orig(Context, Function, Parms, Result);
+		for (auto i = 0; i < numVarLinks; i++)
+		{
+			auto numVars = seqLog->VariableLinks(i).LinkedVariables.Count;
+			for (auto j = 0; j < numVars; j++)
+			{
+				auto seqVar = seqLog->VariableLinks(i).LinkedVariables(j);
+				PrintSequenceVariable(seqVar);
+			}
+		}
+
+		wstring msg = ss.str();
+		writeln(L"SeqAct_Log: %s", msg.c_str());
+		if (!msg.empty())
+		{
+			logger.writeToLog(msg.c_str(), true, true);
+			screenLogger.LogMessage(msg);
+		}
+	}
+	else if (!strcmp(Function->GetFullName(), "Function SFXGame.BioHUD.PostRender"))
+	{
+		auto hud = static_cast<ABioHUD*>(Context);
+		screenLogger.PostRenderer(hud);
+	}
+	ProcessEvent_orig(Context, Function, Parms, Result);
 }
 
 
 
 SPI_IMPLEMENT_ATTACH
 {
-    Common::OpenConsole();
+	Common::OpenConsole();
 
-    auto _ = SDKInitializer::Instance();
+	auto _ = SDKInitializer::Instance();
 
-    INIT_FIND_PATTERN_POSTHOOK(ProcessEvent, /* 40 55 41 56 41 */ "57 48 81 EC 90 00 00 00 48 8D 6C 24 20");
+	INIT_FIND_PATTERN_POSTHOOK(ProcessEvent, /* 40 55 41 56 41 */ "57 48 81 EC 90 00 00 00 48 8D 6C 24 20");
 
-    if (auto rc = InterfacePtr->InstallHook(MYHOOK "ProcessEvent", ProcessEvent, ProcessEvent_hook, (void**)&ProcessEvent_orig); 
-        rc != SPIReturn::Success)
-    {
-        writeln(L"Attach - failed to hook ProcessEvent: %d / %s", rc, SPIReturnToString(rc));
-        return false;
-    }
+	if (auto rc = InterfacePtr->InstallHook(MYHOOK "ProcessEvent", ProcessEvent, ProcessEvent_hook, (void**)&ProcessEvent_orig);
+		rc != SPIReturn::Success)
+	{
+		writeln(L"Attach - failed to hook ProcessEvent: %d / %s", rc, SPIReturnToString(rc));
+		return false;
+	}
 
-    return true;
+	return true;
 }
 
 SPI_IMPLEMENT_DETACH
 {
-    Common::CloseConsole();
-    return true;
+	Common::CloseConsole();
+	return true;
 }
