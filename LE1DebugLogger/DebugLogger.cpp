c3051a31bcfefde0bc4a644c3d7f68e1e68551aa
Mgamerz <mgamerz2@gmail.com>
2021-12-10 08:45:04 -0700

    Update DebugLogger version

--- LE1DebugLogger/DebugLogger.cpp
+++ LE1DebugLogger/DebugLogger.cpp
@@ -7,8 +7,7 @@
 #include "../LE1-SDK/ME3TweaksHeader.h"
 #define MYHOOK "DebugLogger_"
 
-SPI_PLUGINSIDE_SUPPORT(L"DebugLogger", L"1.0.0", L"ME3Tweaks", SPI_GAME_LE1 | SPI_GAME_LE2 | SPI_GAME_LE3,
-                       SPI_VERSION_ANY);
+SPI_PLUGINSIDE_SUPPORT(L"DebugLogger", L"2.0.0", L"ME3Tweaks", SPI_GAME_LE1, SPI_VERSION_ANY);
 SPI_PLUGINSIDE_PRELOAD;
 SPI_PLUGINSIDE_SEQATTACH;
 
