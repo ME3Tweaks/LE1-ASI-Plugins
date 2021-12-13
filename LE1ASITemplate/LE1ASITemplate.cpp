1d858fc5c9543b4e1850f4a54cfa074f176ae1d0
HenBagle <ben.hagle@gmail.com>
2021-12-13 12:29:03 -0500

    Add template project .zip, change LE1MedigelTest to blank ASI project that can be used as a template.

---
+++ LE1ASITemplate/LE1ASITemplate.cpp
@@ -0,0 +1,55 @@
+#include "../LE1-SDK/Interface.h"
+#include "../LE1-SDK/Common.h"
+#include "../LE1-SDK/SdkHeaders.h"
+
+
+#define MYHOOK "HookName_"
+
+SPI_PLUGINSIDE_SUPPORT(L"HookName", L"1.0.0", L"AuthorName", SPI_GAME_LE1, 2);
+SPI_PLUGINSIDE_POSTLOAD;
+SPI_PLUGINSIDE_ASYNCATTACH;
+
+
+
+// ProcessEvent hook
+// ======================================================================
+
+typedef void (*tProcessEvent)(UObject* Context, UFunction* Function, void* Parms, void* Result);
+tProcessEvent ProcessEvent = nullptr;
+
+tProcessEvent ProcessEvent_orig = nullptr;
+void ProcessEvent_hook(UObject* Context, UFunction* Function, void* Parms, void* Result)
+{
+    ProcessEvent_orig(Context, Function, Parms, Result);
+}
+
+
+SPI_IMPLEMENT_ATTACH
+{
+    Common::OpenConsole();
+
+    auto _ = SDKInitializer::Instance();
+
+    if (auto rc = InterfacePtr->FindPattern((void**)&ProcessEvent, "40 55 41 56 41 57 48 81 EC 90 00 00 00 48 8D 6C 24 20"); 
+        rc != SPIReturn::Success)
+    {
+        writeln(L"Attach - failed to find ProcessEvent pattern: %d / %s", rc, SPIReturnToString(rc));
+        return false;
+    }
+
+
+    if (auto rc = InterfacePtr->InstallHook(MYHOOK "ProcessEvent", ProcessEvent, ProcessEvent_hook, (void**)&ProcessEvent_orig); 
+        rc != SPIReturn::Success)
+    {
+        writeln(L"Attach - failed to hook ProcessEvent: %d / %s", rc, SPIReturnToString(rc));
+        return false;
+    }
+
+    return true;
+}
+
+SPI_IMPLEMENT_DETACH
+{
+    Common::CloseConsole();
+    return true;
+}
