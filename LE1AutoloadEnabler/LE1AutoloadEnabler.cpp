fde72f3f23458a485fa900493349b314311ffc05
Mgamerz <mgamerz2@gmail.com>
2022-02-12 00:24:42 -0700

    Fix working directory being wrong when not autobooted from launcher

--- LE1AutoloadEnabler/LE1AutoloadEnabler.cpp
+++ LE1AutoloadEnabler/LE1AutoloadEnabler.cpp
@@ -3,6 +3,7 @@
 
 #include <filesystem>
 #include <Shlwapi.h>
+#include <thread>
 #include <vector>
 #include <Windows.h>
 #include "../LE1-SDK/Interface.h"
@@ -11,16 +12,24 @@
 #include "ExtraContent.h"
 #include "Logging.h"
 
-#define MYHOOK "LE1AutoloadIniLogger_"
+#define MYHOOK "LE1AutoloadEnabler"
 #ifndef NDEBUG
 constexpr bool GIsRelease = false;
 #else
 constexpr bool GIsRelease = true;
 #endif
 
-SPI_PLUGINSIDE_SUPPORT(L"LE1AutoloadIniLogger", L"---", L"0.3.0", SPI_GAME_LE1, SPI_VERSION_LATEST);
+SPI_PLUGINSIDE_SUPPORT(L"LE1AutoloadEnabler", L"---", L"0.4.0", SPI_GAME_LE1, SPI_VERSION_LATEST);
 SPI_PLUGINSIDE_POSTLOAD;
-SPI_PLUGINSIDE_ASYNCATTACH;
+SPI_PLUGINSIDE_SEQATTACH;
+
+// Fixes bad launcher logic when not using Autoboot (sets the wrong working directory)
+void SetWorkingDirectory() {
+	WCHAR path[MAX_PATH];
+	GetModuleFileNameW(NULL, path, MAX_PATH);
+	std::filesystem::path exePath = path;
+	SetCurrentDirectoryW(exePath.parent_path().c_str());
+}
 
 
 // Functions used to make a list of available DLCs.
@@ -63,36 +72,29 @@ std::vector<std::wstring> GetAllDLCAutoloads(std::wstring&& searchRoot)
 	return autoloadPaths;
 }
 
-// RegisterTFC prototype so we can call it when necessary
+// HasAtLeastOneBasegameTFC: So we know when we can call RegisterTFC
+typedef bool (*tNoParamBool)();
+tNoParamBool HasAtLeastOneBasegameTFC = nullptr;
+
+// ======================================================================
+// RegisterTFC prototype so we can call it to register our TFCs
 // =====================================================================
 typedef void (*tRegisterTFC)(FString* name);
 tRegisterTFC RegisterTFC = nullptr;
-tRegisterTFC RegisterTFC_orig = nullptr;
 
+// The list of pending TFCs to register once our first package file tries to load
 std::vector<FString*> DLCTFCsToRegister;
-bool registeredFirstTFC = false;
 
 // Logs a message and registers a TFC
 void RegisterTFCWrapper(FString* tfcPath)
 {
 	writeln(L"Registering DLC mod TFC file: %s", tfcPath->Data);
-	RegisterTFC_orig(tfcPath);
-	free(tfcPath->Data); // Data consturcted here was made with _wcsdup
+	RegisterTFC(tfcPath);
+	// This just crashes it idk why
+	//free(tfcPath->Data); // Data consturcted here was made with _wcsdup
 }
 
-void RegisterTFC_hook(FString* tfc)
-{
-	if (!registeredFirstTFC)
-	{
-		registeredFirstTFC = true;
-		for each (FString* tfcPath in DLCTFCsToRegister)
-		{
-			RegisterTFCWrapper(tfcPath);
-		}
-	}
-	RegisterTFC_orig(tfc);
-}
-
+// ======================================================================
 // ProcessIni hook
 // Logs every invocation, call itself for each found Autoload.ini.
 // ======================================================================
@@ -107,7 +109,7 @@ tProcessIni ProcessIni = nullptr;
 tProcessIni ProcessIni_orig = nullptr;
 void ProcessIni_hook(ExtraContent* ExtraContent, FString* IniPath, FString* BasePath)
 {
-	writeln(L"ProcessIni - ExtraContent = %p, IniPath is %s", ExtraContent, IniPath->Data);
+	writeln(L"Registering DLC mod Autoload.ini %s", IniPath->Data);
 	ProcessIni_orig(ExtraContent, IniPath, BasePath);
 
 	if (!GOriginalCalled)
@@ -124,13 +126,11 @@ void ProcessIni_hook(ExtraContent* ExtraContent, FString* IniPath, FString* Base
 
 
 // ======================================================================
-
 // ProcessEvent hook
 // Renders autoload profiler, allows toggling it.
 // ======================================================================
 
 #ifndef NDEBUG
-
 typedef void (*tProcessEvent)(UObject* Context, UFunction* Function, void* Parms, void* Result);
 tProcessEvent ProcessEvent = nullptr;
 
@@ -139,9 +139,7 @@ void ProcessEvent_hook(UObject* Context, UFunction* Function, void* Parms, void*
 {
 	static ExtraContentHUD* hud = nullptr;
 
-
 	// Render autoload profiler HUD.
-
 	if (!strcmp(Function->GetFullName(), "Function SFXGame.BioHUD.PostRender"))
 	{
 		if (!hud)
@@ -154,7 +152,6 @@ void ProcessEvent_hook(UObject* Context, UFunction* Function, void* Parms, void*
 
 
 	// Allow toggling the autoload profiler HUD on `profile autoload` command.
-
 	if (!strcmp(Function->GetFullName(), "Function Console.Typing.InputChar") && Parms && hud)
 	{
 		auto sfxConsole = reinterpret_cast<UConsole*>(Context);
@@ -172,60 +169,136 @@ void ProcessEvent_hook(UObject* Context, UFunction* Function, void* Parms, void*
 
 #endif
 
+#pragma region CacheContent
+// ======================================================================
+// ISB Registration
+// ======================================================================
+
+// List of found ISBs that will register on first ISB registration
+std::vector<wchar_t*> ISBsToRegister;
+
+typedef void (*tCachePackage)(long long parm1, wchar_t* filePath, bool replaceIfExisting, bool warnIfExists);
+tCachePackage CacheContentWrapper = nullptr;
+tCachePackage CacheContentWrapper_orig = nullptr;
+
+bool registeredISBs = false;
+void CacheContentWrapper_hook(long long parm1, wchar_t* filePath, bool replaceIfExisting, bool warnIfExists)
+{
+	if (!registeredISBs)
+	{
+		registeredISBs = true;
+		for each (auto isb in ISBsToRegister)
+		{
+			writeln(L"Registering DLC mod ISB %s", isb);
+			CacheContentWrapper_orig(parm1, isb, true, false);
+			delete isb; // IDK if this is accurate...
+		}
+	}
+	CacheContentWrapper_orig(parm1, filePath, replaceIfExisting, warnIfExists);
+}
+
+#pragma endregion CacheContent
+
+#pragma region FirstLoad
+// ======================================================================
+// This method is called when files are opened for reading (it seems).
+// Core.pcc is the first invocation, which means TFCs should have been registered by now
+// so textures don't try to load before TFCs are available
+// ======================================================================
+
+typedef void* (*tSomethingFirstLoad)(long long* parm1, void* parm2, wchar_t** parm3, void* parm4);
+tSomethingFirstLoad SomethingFirstLoad = nullptr;
+tSomethingFirstLoad SomethingFirstLoad_orig = nullptr;
+int numFilesRead = 0;
+bool firstLoad = true;
+
+void* SomethingFirstLoad_hook(long long* parm1, void* parm2, wchar_t** filePath, void* parm4)
+{
+	if (firstLoad)
+	{
+		firstLoad = false;
+		for each (auto tfcPath in DLCTFCsToRegister)
+		{
+			RegisterTFCWrapper(tfcPath);
+		}
+	}
+
+	// This call seems to be for when files are opened for reading (CreateFileW is called). This happens a lot with things like TFC reading but first hit is Core.pcc
+	return SomethingFirstLoad_orig(parm1, parm2, filePath, parm4);
+}
+
+#pragma endregion FirstLoad
+
 // ======================================================================
 
 SPI_IMPLEMENT_ATTACH
 {
-		initLog();
-		//writeln(L"Attach - hello there!");
+	SetWorkingDirectory();
+	initLog();
 
-		// Find RegisterTFC so we can register TFCs
-		// As TFCs are one of the first things loaded this must be done ASAP
-		INIT_FIND_PATTERN_POSTHOOK(RegisterTFC, /*48 8b c4 57 41*/ "56 41 57 48 83 ec 60 48 c7 40 a8 fe ff ff ff 48 89 58 10 48 89 68 18 48 89 70 20 4c 8b f9 48 8b 0d 6e 3e 45 01 48 8b 01 48 8d 2d b0 9d d4 00 41 83 7f 08 00 74 05 49 8b 17 eb 03");
-		INIT_HOOK_PATTERN(RegisterTFC); // We will load our TFCs first
+	// Find RegisterTFC so we can register TFCs
+	INIT_FIND_PATTERN_POSTHOOK(RegisterTFC, /*48 8b c4 57 41*/ "56 41 57 48 83 ec 60 48 c7 40 a8 fe ff ff ff 48 89 58 10 48 89 68 18 48 89 70 20 4c 8b f9 48 8b 0d 6e 3e 45 01 48 8b 01 48 8d 2d b0 9d d4 00 41 83 7f 08 00 74 05 49 8b 17 eb 03");
 
-#ifndef NDEBUG
-		// Initialize the SDK because we need object names.
-		INIT_CHECK_SDK();
+	// Leftover for documentation sake.
+	// We need to find 'Has at least one TFC' which I think means TFC manager has loaded...
+	//INIT_FIND_PATTERN_POSTHOOK(HasAtLeastOneBasegameTFC, /* 8b 0d aa a6 50 */ "01 33 c0 2b 0d ce a6 50 01 85 c9 0f 9f c0 c3"); // dunno if this is long enough
+	
+	INIT_FIND_PATTERN_POSTHOOK(CacheContentWrapper, /*48 8b c4 55 41*/ "54 41 55 41 56 41 57 48 8d 68 a1 48 81 ec 00 01 00 00 48 c7 45 27 fe ff ff ff 48 89 58 08 48 89 70 10 48 89 78 18 45 8b e9");
+	INIT_HOOK_PATTERN(CacheContentWrapper); // For ISB registration
 
-		// Hook ProcessEvent for debugging.
-		INIT_FIND_PATTERN(ProcessEvent, "40 55 41 56 41 57 48 81 EC 90 00 00 00 48 8D 6C 24 20");
-		INIT_HOOK_PATTERN(ProcessEvent);
-#endif
+	// This method is called initially when Core.pcc loads
+	INIT_FIND_PATTERN_POSTHOOK(SomethingFirstLoad, /*48 8b c4 56 57*/ "41 54 41 56 41 57 48 83 ec 50 48 c7 40 a8 fe ff ff ff 48 89 58 10 48 89 68 18 4d 8b f0 48 8b f2");
+	INIT_HOOK_PATTERN(SomethingFirstLoad);
 
-		// Find and hook the ProcessIni.
-		INIT_FIND_PATTERN(ProcessIni, "40 55 56 57 41 54 41 55 41 56 41 57 48 8D AC 24 A0 EC FF FF B8 60 14 00 00");
-		INIT_HOOK_PATTERN(ProcessIni);
+	#ifndef NDEBUG
+	// Initialize the SDK because we need object names.
+	INIT_CHECK_SDK();
 
-		// Get a list of DLC Autoloads.
+	// Hook ProcessEvent for debugging.
+	INIT_FIND_PATTERN(ProcessEvent, "40 55 41 56 41 57 48 81 EC 90 00 00 00 48 8D 6C 24 20");
+	INIT_HOOK_PATTERN(ProcessEvent);
+	#endif
 
-		for (const auto& autoload : GetAllDLCAutoloads(GetDLCsRoot()))
+	// Find and hook the ProcessIni for Autoload.ini support
+	INIT_FIND_PATTERN(ProcessIni, "40 55 56 57 41 54 41 55 41 56 41 57 48 8D AC 24 A0 EC FF FF B8 60 14 00 00");
+	INIT_HOOK_PATTERN(ProcessIni);
+
+	// Get a list of DLC Autoloads.
+	writeln("Finding DLC content...");
+	for (const auto& autoload : GetAllDLCAutoloads(GetDLCsRoot()))
+	{
+		writeln(L"Found DLC Autoload.ini: %s", autoload.c_str());
+		GExtraAutoloadPaths.push_back(autoload.c_str());
+
+		// It's a DLC mod, LE only has a single autoload.ini (Bring Down The Sky)
+		std::filesystem::path path = autoload.c_str();
+		auto dlcFolder = path.parent_path();
+
+		for (const auto& entry : std::filesystem::recursive_directory_iterator(dlcFolder))
 		{
-			writeln(L"Attach - found a DLC autoload: %s", autoload.c_str());
-			GExtraAutoloadPaths.push_back(autoload.c_str());
-
-			// It's a DLC mod, LE only has a single autoload.ini (Bring Down The Sky)
-			std::filesystem::path path = autoload.c_str();
-			auto dlcFolder = path.parent_path();
-			for (const auto& entry : std::filesystem::recursive_directory_iterator(dlcFolder))
+			if (entry.is_directory())
 			{
-				if (entry.is_directory())
-					continue;
+				writeln(L"\tScanning %s", entry.path().c_str());
+				continue;
+			}
 
-				if (entry.path().extension() == L".tfc") {
-					// Due to startup timing issues we have to do this
-					if (registeredFirstTFC)
-					{
-						// This one won't be in list so we have to register it now.
-						RegisterTFCWrapper(new FString(_wcsdup(entry.path().c_str())));
-					}
-					else
-					{
-						DLCTFCsToRegister.push_back(new FString(_wcsdup(entry.path().c_str()))); // We have to wait until first registration attempt or we'll hit a null pointer
-					}
-				}
+			// _wcsdup might leak memory but not much. Freeing it upon consumption crashes the app so...
+			auto extension = entry.path().extension();
+			if (extension == L".tfc") {
+				auto path = entry.path().c_str();
+				writeln(L"\t\tFound TFC: %s", path);
+				DLCTFCsToRegister.push_back(new FString(_wcsdup(path))); // We have to wait until first registration attempt or we'll hit a null pointer
+			}
+			else if (extension == ".isb")
+			{
+				// Register ISB
+				auto path = entry.path().c_str();
+				writeln(L"\t\tFound ISB: %s", path);
+				ISBsToRegister.push_back(_wcsdup(path)); // We have to wait until first registration attempt or we'll hit a null pointer
 			}
 		}
+	}
+	writeln("Completed DLC content detection");
 	return true;
 }
 
