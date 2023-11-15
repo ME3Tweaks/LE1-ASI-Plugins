0ccc92525be17b9f66360009822816672ae6fb30
Mgamerz <mgamerz@msn.com>
2023-11-14 20:31:50 -0700

    Fix wrong path being logged when ini is mounted. Fix DLC ini's not bound mounted due to change in how paths are returned

--- LE1AutoloadEnabler/LE1AutoloadEnabler.cpp
+++ LE1AutoloadEnabler/LE1AutoloadEnabler.cpp
@@ -174,7 +174,7 @@ void ProcessIni_hook(ExtraContent* ExtraContent, FString* IniPath, FString* Base
 		GOriginalCalled = true;
 		for (const auto& autoloadPath : GExtraAutoloadPaths)
 		{
-			writeln(L"Registering DLC mod Autoload.ini %s", IniPath->Data);
+			writeln(L"Registering DLC mod Autoload.ini %s", autoloadPath.c_str());
 			ProcessIni(ExtraContent, &FString{ const_cast<wchar_t*>(autoloadPath.c_str()) }, nullptr);
 		}
 		GExtraContent = ExtraContent;
@@ -208,7 +208,7 @@ void RegisterStartupFile(UPackage* package)
 			{
 				// It's a child of this package!
 				writeln(L"Rooting startup package object %hs in package %hs", obj->GetFullName(), package->Name.GetName())
-				RootObject(obj);
+					RootObject(obj);
 			}
 		}
 	}
@@ -383,94 +383,96 @@ SPI_IMPLEMENT_ATTACH
 	SetWorkingDirectory(); // This need
 	initLog();
 
-// Find RegisterTFC so we can register TFCs
-INIT_FIND_PATTERN_POSTHOOK(RegisterTFC, /*48 8b c4 57 41*/ "56 41 57 48 83 ec 60 48 c7 40 a8 fe ff ff ff 48 89 58 10 48 89 68 18 48 89 70 20 4c 8b f9 48 8b 0d 6e 3e 45 01 48 8b 01 48 8d 2d b0 9d d4 00 41 83 7f 08 00 74 05 49 8b 17 eb 03");
+	// Find RegisterTFC so we can register TFCs
+	INIT_FIND_PATTERN_POSTHOOK(RegisterTFC, /*48 8b c4 57 41*/ "56 41 57 48 83 ec 60 48 c7 40 a8 fe ff ff ff 48 89 58 10 48 89 68 18 48 89 70 20 4c 8b f9 48 8b 0d 6e 3e 45 01 48 8b 01 48 8d 2d b0 9d d4 00 41 83 7f 08 00 74 05 49 8b 17 eb 03");
 
-// Leftover for documentation sake.
-// We need to find 'Has at least one TFC' which I think means TFC manager has loaded...
-//INIT_FIND_PATTERN_POSTHOOK(HasAtLeastOneBasegameTFC, /* 8b 0d aa a6 50 */ "01 33 c0 2b 0d ce a6 50 01 85 c9 0f 9f c0 c3"); // dunno if this is long enough
+	// Leftover for documentation sake.
+	// We need to find 'Has at least one TFC' which I think means TFC manager has loaded...
+	//INIT_FIND_PATTERN_POSTHOOK(HasAtLeastOneBasegameTFC, /* 8b 0d aa a6 50 */ "01 33 c0 2b 0d ce a6 50 01 85 c9 0f 9f c0 c3"); // dunno if this is long enough
 
-INIT_FIND_PATTERN_POSTHOOK(CacheContentWrapper, /*48 8b c4 55 41*/ "54 41 55 41 56 41 57 48 8d 68 a1 48 81 ec 00 01 00 00 48 c7 45 27 fe ff ff ff 48 89 58 08 48 89 70 10 48 89 78 18 45 8b e9");
-INIT_HOOK_PATTERN(CacheContentWrapper); // For ISB registration
+	INIT_FIND_PATTERN_POSTHOOK(CacheContentWrapper, /*48 8b c4 55 41*/ "54 41 55 41 56 41 57 48 8d 68 a1 48 81 ec 00 01 00 00 48 c7 45 27 fe ff ff ff 48 89 58 08 48 89 70 10 48 89 78 18 45 8b e9");
+	INIT_HOOK_PATTERN(CacheContentWrapper); // For ISB registration
 
-// This method is called initially when Core.pcc loads
-INIT_FIND_PATTERN_POSTHOOK(SomethingFirstLoad, /*48 8b c4 56 57*/ "41 54 41 56 41 57 48 83 ec 50 48 c7 40 a8 fe ff ff ff 48 89 58 10 48 89 68 18 4d 8b f0 48 8b f2");
-INIT_HOOK_PATTERN(SomethingFirstLoad);
+	// This method is called initially when Core.pcc loads
+	INIT_FIND_PATTERN_POSTHOOK(SomethingFirstLoad, /*48 8b c4 56 57*/ "41 54 41 56 41 57 48 83 ec 50 48 c7 40 a8 fe ff ff ff 48 89 58 10 48 89 68 18 4d 8b f0 48 8b f2");
+	INIT_HOOK_PATTERN(SomethingFirstLoad);
 
-// Initialize the SDK because we need object names.
-INIT_CHECK_SDK();
+	// Initialize the SDK because we need object names.
+	INIT_CHECK_SDK();
 
-// This is done first cause it's kind of important that it's done early so changes to things like
-// BIOC_Materials work
+	// This is done first cause it's kind of important that it's done early so changes to things like
+	// BIOC_Materials work
 
-// Get a list of DLC Autoloads.
-writeln(L"Finding DLC content in %s...", GetDLCsRoot().c_str());
+	// Get a list of DLC Autoloads.
+	writeln(L"Finding DLC content in %s...", GetDLCsRoot().c_str());
 
-std::map<int, std::wstring> dlcMountOrder;
+	std::map<int, std::wstring> dlcMountOrder;
 
-std::wstring dlcRoot = GetDLCsRoot();
-GetLE1DLCMountOrder(dlcMountOrder, dlcRoot.c_str());
-for (const auto& autoload : dlcMountOrder) // This has weird error 
-{
-	TCHAR tmpPath[MAX_PATH];
-
-	writeln(L"Found DLC Autoload.ini: %s, mount %i", autoload.second.c_str(), autoload.first);
-	GExtraAutoloadPaths.emplace_back(autoload.second.c_str());
-
-	// It's a DLC mod. LE only has a single autoload.ini (Bring Down The Sky) and it's not in the DLC folder
-	StringCchCopy(tmpPath, MAX_PATH, dlcRoot.c_str());
-	StringCchCat(tmpPath, MAX_PATH, autoload.second.c_str());
-
-	for (const auto& entry : std::filesystem::recursive_directory_iterator(tmpPath))
+	std::wstring dlcRoot = GetDLCsRoot();
+	GetLE1DLCMountOrder(dlcMountOrder, dlcRoot.c_str());
+	for (const auto& autoload : dlcMountOrder) // This has weird error 
 	{
-		if (entry.is_directory())
-		{
-			writeln(L"\tScanning %s", entry.path().c_str());
-			continue;
-		}
+		TCHAR tmpPath[MAX_PATH];
 
-		// _wcsdup might leak memory but not much. Freeing it upon consumption crashes the app so...
-		auto extension = entry.path().extension();
-		if (extension == L".tfc") {
-			auto tfcPath = entry.path().c_str();
-			writeln(L"\t\tFound TFC: %s", tfcPath);
-			DLCTFCsToRegister.push_back(new FString(_wcsdup(tfcPath))); // We have to wait until first registration attempt or we'll hit a null pointer
-		}
-		else if (extension == L".isb")
+		// It's a DLC mod. LE only has a single autoload.ini (Bring Down The Sky) and it's not in the DLC folder
+		StringCchCopy(tmpPath, MAX_PATH, dlcRoot.c_str());
+		StringCchCat(tmpPath, MAX_PATH, autoload.second.c_str());
+		auto fileIterator = std::filesystem::recursive_directory_iterator(tmpPath);
+		StringCchCat(tmpPath, MAX_PATH, L"\\Autoload.ini");
+
+		writeln(L"Found DLC Autoload.ini: %s, mount %i", tmpPath, autoload.first);
+		GExtraAutoloadPaths.emplace_back(tmpPath);
+
+		for (const auto& entry : fileIterator)
 		{
-			// Register ISB
-			auto isbPath = entry.path().c_str();
-			writeln(L"\t\tFound ISB: %s", isbPath);
-			ISBsToRegister.push_back(_wcsdup(isbPath)); // We have to wait until first registration attempt or we'll hit a null pointer
+			if (entry.is_directory())
+			{
+				writeln(L"\tScanning %s", entry.path().c_str());
+				continue;
+			}
+
+			// _wcsdup might leak memory but not much. Freeing it upon consumption crashes the app so...
+			auto extension = entry.path().extension();
+			if (extension == L".tfc") {
+				auto tfcPath = entry.path().c_str();
+				writeln(L"\t\tFound TFC: %s", tfcPath);
+				DLCTFCsToRegister.push_back(new FString(_wcsdup(tfcPath))); // We have to wait until first registration attempt or we'll hit a null pointer
+			}
+			else if (extension == L".isb")
+			{
+				// Register ISB
+				auto isbPath = entry.path().c_str();
+				writeln(L"\t\tFound ISB: %s", isbPath);
+				ISBsToRegister.push_back(_wcsdup(isbPath)); // We have to wait until first registration attempt or we'll hit a null pointer
+			}
 		}
 	}
-}
-ContentScanComplete = true;
-writeln(L"Completed DLC content detection");
+	ContentScanComplete = true;
+	writeln(L"Completed DLC content detection");
 
-// Hook stuff that won't be needed until like 10-15 seconds into the game
+	// Hook stuff that won't be needed until like 10-15 seconds into the game
 
-#if !NDEBUG
-	// Hook ProcessEvent for debugging.
-	INIT_FIND_PATTERN_POSTHOOK(ProcessEvent, LE_PATTERN_POSTHOOK_PROCESSEVENT);
-	INIT_HOOK_PATTERN(ProcessEvent);
-#endif
+	#if !NDEBUG
+		// Hook ProcessEvent for debugging.
+		INIT_FIND_PATTERN_POSTHOOK(ProcessEvent, LE_PATTERN_POSTHOOK_PROCESSEVENT);
+		INIT_HOOK_PATTERN(ProcessEvent);
+	#endif
 
-	// Find and hook the ProcessIni for Autoload.ini support
-	INIT_FIND_PATTERN_POSTHOOK(ProcessIni, /*"40 55 56 57 41*/ "54 41 55 41 56 41 57 48 8D AC 24 A0 EC FF FF B8 60 14 00 00");
-	INIT_HOOK_PATTERN(ProcessIni);
+		// Find and hook the ProcessIni for Autoload.ini support
+		INIT_FIND_PATTERN_POSTHOOK(ProcessIni, /*"40 55 56 57 41*/ "54 41 55 41 56 41 57 48 8D AC 24 A0 EC FF FF B8 60 14 00 00");
+		INIT_HOOK_PATTERN(ProcessIni);
 
-	INIT_FIND_PATTERN_POSTHOOK(InstallDownloadableContent, /*"48 8b c4 55 41*/ "54 41 55 41 56 41 57 48 8d a8 98 fe ff ff 48 81 ec 40 02 00 00 48 c7 45 18 fe ff ff ff");
-	INIT_HOOK_PATTERN(InstallDownloadableContent);
+		INIT_FIND_PATTERN_POSTHOOK(InstallDownloadableContent, /*"48 8b c4 55 41*/ "54 41 55 41 56 41 57 48 8d a8 98 fe ff ff 48 81 ec 40 02 00 00 48 c7 45 18 fe ff ff ff");
+		INIT_HOOK_PATTERN(InstallDownloadableContent);
 
-	// OBJECT ROOTING FOR STARTUP FILE OBJECTS
-	// NOTE: THIS POINTS TO A FUNCTION FOLLOWING BY 0X10 AS THE FUNC IS TOO SMALL TO FIND BY SIGNATURE
-	INIT_FIND_PATTERN_POSTHOOK(RootObject, /*"40 55 48 8b ec*/"48 83 ec 50 48 c7 45 e0 fe ff ff ff 48 89 5c 24 60");
-	//writeln("RootObject currently at 0x%p", RootObject);
-	RootObject = (tUObjectRoot)((char*)RootObject - 0x10);
-	//writeln("Actual RootObject is at 0x%p", RootObject);
+		// OBJECT ROOTING FOR STARTUP FILE OBJECTS
+		// NOTE: THIS POINTS TO A FUNCTION FOLLOWING BY 0X10 AS THE FUNC IS TOO SMALL TO FIND BY SIGNATURE
+		INIT_FIND_PATTERN_POSTHOOK(RootObject, /*"40 55 48 8b ec*/"48 83 ec 50 48 c7 45 e0 fe ff ff ff 48 89 5c 24 60");
+		//writeln("RootObject currently at 0x%p", RootObject);
+		RootObject = (tUObjectRoot)((char*)RootObject - 0x10);
+		//writeln("Actual RootObject is at 0x%p", RootObject);
 
-	return true;
+		return true;
 }
 
 SPI_IMPLEMENT_DETACH
